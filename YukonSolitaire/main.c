97fac4afd96bd3479437faf7c2e0e792052f6ce2
sheree <s173750@student.dtu.dk>
2022-04-11 17:48:42 +0200

    defining a suit of cards

--- YukonSolitaire/main.c
+++ YukonSolitaire/main.c
@@ -15,6 +15,10 @@ typedef struct node {
 
 int main () {
 
+    char suitStr [13] = { 'A', '2', '3', '4', '5', '6',
+                          '7', '8', '9', 'T', 'J', 'Q', 'K'};
+    char faceStr [4] = {'C', 'D', 'H', 'K'};
+
 
     return 0;
 }
