3f7bc2282fbff6c4841117c3470cc5d302e01415
sheree <s173750@student.dtu.dk>
2022-04-11 17:43:06 +0200

    defined a single node

--- YukonSolitaire/main.c
+++ YukonSolitaire/main.c
@@ -1,46 +1,20 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include  <stdlib.h>
+#include <string.h>
 
 
-struct node {
-    int value;
+typedef struct node {
+
+    char suit;
+    char face;
+    int hidden;
 
     struct node* next;
 
-};
+}node;
 
-typedef struct node node_t;
+int main () {
 
-
-void printlist(node_t *head) {
-
-    node_t *temporary = head;
-
-    while (temporary != NULL) {
-
-        printf("%d - ", temporary->value);
-        temporary = temporary->next;
-    }
-    printf("\n");
-}
-
-int main() {
-
-    node_t n1, n2, n3;
-    node_t *head;
-
-    n1.value  = 20;
-    n2.value = 21;
-    n3.value  =22;
-
-
-    //linking the nodes up in a list
-    head = &n3;
-    n3.next = &n2;
-    n2.next = &n1;
-    n1.next = NULL;
-
-    printlist(head);
 
     return 0;
 }
