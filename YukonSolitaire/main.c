e236813fd5ca9218d7697eefcf916c3fe658893b
Rasm-P <rasmus.pra@gmail.com>
2022-04-15 15:47:43 +0200

    Fixed some problems with insertBlocks and the SD function

--- YukonSolitaire/main.c
+++ YukonSolitaire/main.c
@@ -11,17 +11,17 @@ typedef struct node {
     struct node* next;
 }node;
 
-int LD(char lastCommand[]);
+char suitStr [13] = { 'A', '2', '3', '4', '5', '6',
+                      '7', '8', '9', 'T', 'J', 'Q', 'K'};
+char faceStr [4] = {'C', 'D', 'H', 'S'};
+
+int LD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4);
 int SD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4) ;
 void insertElement(node** root, char suit, char face, int hidden);
 void insertCardDeck(node* cardDeck);
 void insertBlocks(char suitStr[], char faceStr[], node* C1, node* C2, node* C3, node* C4, node* C5, node* C6, node* C7);
 
 int main() {
-    char suitStr [13] = { 'A', '2', '3', '4', '5', '6',
-                          '7', '8', '9', 'T', 'J', 'Q', 'K'};
-    char faceStr [4] = {'C', 'D', 'H', 'K'};
-
     node* ushuffledDeck = malloc(sizeof(node));
     ushuffledDeck -> next = NULL;
     insertCardDeck(ushuffledDeck);
@@ -50,9 +50,6 @@ int main() {
     node* foundation4 = malloc(sizeof(node));
     foundation4 -> next = NULL;
 
-    insertBlocks(suitStr, faceStr, C1, C2, C3, C4, C5, C6, C7);
-
-
     bool isRunning = true;
     char lastCommand[MAX_STRING] = {0};
     int result;
@@ -60,7 +57,7 @@ int main() {
 
     while (isRunning) {
         if (lastCommand[0] == 'L' && lastCommand[1] == 'D') {
-            result = LD(lastCommand);
+            result = LD(lastCommand, C1,C2,C3,C4,C5,C6,C7,foundation1,foundation2,foundation3,foundation4);
         } else if (lastCommand[0] == 'S' && lastCommand[1] == 'W') {
             printf("TO DO!\n");
         } else if (lastCommand[0] == 'S' && lastCommand[1] == 'L') {
@@ -95,7 +92,7 @@ int main() {
     return 0;
 }
 
-int LD(char lastCommand[]) {
+int LD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4) {
     if (lastCommand[2] == '<') {
         char optionalParameter[MAX_STRING];
         int j = 0;
@@ -122,7 +119,7 @@ int LD(char lastCommand[]) {
             return -1;
         }
     } else {
-        printf("TO DO!\n");
+        insertBlocks(suitStr, faceStr, C1, C2, C3, C4, C5, C6, C7);
     }
     return 0;
 }
@@ -147,67 +144,67 @@ int SD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, nod
     if (outfile != NULL) {
         node* current;
         current = C1 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = C2 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = C3 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = C4 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = C5 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = C6 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = C7 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = foundation1 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = foundation2 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = foundation3 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
         fprintf(outfile,"-\n");
         current = foundation4 -> next;
-        while (current != NULL && current -> next != NULL) {
+        while (current != NULL) {
             fprintf(outfile, "%c%c%d.\n",current->suit,current->face,current->hidden);
             current = current -> next;
         }
@@ -253,50 +250,65 @@ void insertBlocks(char suitStr[], char faceStr[], node* C1, node* C2, node* C3, 
 
     int k = 51;
     int i = 0;
+    int j = 0;
 
     while ( i < 52 ) {
-        insertElement( &C1, suitStr [i % 13], faceStr [i % 4], 0);
+        insertElement( &C1, suitStr [i % 13], faceStr [j], 0);
         i++;
 
         if (i > k) {
             break;
+        } else if (i % 13 == 0) {
+            j++;
         }
-        insertElement( &C2, suitStr [ i % 13], faceStr [i % 4], 0);
+        insertElement( &C2, suitStr [ i % 13], faceStr [j], 0);
         i++;
 
         if (i > k) {
             break;
+        } else if (i % 13 == 0) {
+            j++;
         }
-        insertElement(&C3, suitStr [i % 13], faceStr [i % 4], 0);
+        insertElement(&C3, suitStr [i % 13], faceStr [j], 0);
         i++;
 
         if (i > k) {
             break;
 
+        } else if (i % 13 == 0) {
+            j++;
         }
-        insertElement(&C4, suitStr [i % 13], faceStr [i % 4], 0);
+        insertElement(&C4, suitStr [i % 13], faceStr [j], 0);
         i++;
 
         if (i > k )  {
             break;
+        } else if (i % 13 == 0) {
+            j++;
         }
-        insertElement(&C5, suitStr [i % 13], faceStr [i % 4], 0);
+        insertElement(&C5, suitStr [i % 13], faceStr [j], 0);
         i++;
 
         if (i > k) {
             break;
+        } else if (i % 13 == 0) {
+            j++;
         }
-        insertElement(&C6, suitStr[i % 13], faceStr [i % 4], 0);
+        insertElement(&C6, suitStr[i % 13], faceStr [j], 0);
         i++;
 
         if (i > k) {
             break;
+        } else if (i % 13 == 0) {
+            j++;
         }
-        insertElement(&C7, suitStr [i % 13], faceStr [i % 4], 0);
+        insertElement(&C7, suitStr [i % 13], faceStr [j], 0);
         i++;
 
         if (i > k) {
             break;
+        } else if (i % 13 == 0) {
+            j++;
         }
     }
 }
