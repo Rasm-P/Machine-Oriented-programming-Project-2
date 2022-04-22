c30b0a956ea3272ecebdcc478527207dd9c2de77
Rasm-P <rasmus.pra@gmail.com>
2022-04-22 15:19:15 +0200

    Fixed some issues for LD and SD, added L and S functions, and added an unfload function

--- main.c
+++ main.c
@@ -14,17 +14,20 @@ typedef struct node {
 char suitStr [13] = { 'A', '2', '3', '4', '5', '6',
                       '7', '8', '9', 'T', 'J', 'Q', 'K'};
 char faceStr [4] = {'C', 'D', 'H', 'S'};
+int STARTUP = 1;
 
-int LD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4, char **resultMessage);
-int SD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4, char **resultMessage) ;
+int LD(char lastCommand[], node *cardDeck, char **resultMessage);
+int SD(char lastCommand[], node *cardDeck, char **resultMessage);
+void unloadCards(node* cards);
 void insertElement(node** root, char suit, char face, int hidden);
 void insertCardDeck(node* cardDeck);
+int L(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4, char **resultMessage);
+int S(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4, char **resultMessage);
 void insertBlocks(char suitStr[], char faceStr[], node* C1, node* C2, node* C3, node* C4, node* C5, node* C6, node* C7);
 
 int main() {
-    node* ushuffledDeck = malloc(sizeof(node));
-    ushuffledDeck -> next = NULL;
-    insertCardDeck(ushuffledDeck);
+    node* cardDeck = malloc(sizeof(node));
+    cardDeck -> next = NULL;
 
     node* C1 = malloc(sizeof(node));
     C1 -> next = NULL;
@@ -56,26 +59,38 @@ int main() {
     char *resultMessage = (char*) malloc(sizeof(char) * MAX_STRING);
 
     while (isRunning) {
-        if (lastCommand[0] == 'L' && lastCommand[1] == 'D') {
-            result = LD(lastCommand, C1,C2,C3,C4,C5,C6,C7,foundation1,foundation2,foundation3,foundation4, &resultMessage);
-        } else if (lastCommand[0] == 'S' && lastCommand[1] == 'W') {
-            printf("TO DO!\n");
-        } else if (lastCommand[0] == 'S' && lastCommand[1] == 'L') {
-            printf("TO DO!\n");
-        } else if (lastCommand[0] == 'S' && lastCommand[1] == 'R') {
-            printf("TO DO!\n");
-        } else if (lastCommand[0] == 'S' && lastCommand[1] == 'D') {
-            result = SD(lastCommand, C1,C2,C3,C4,C5,C6,C7,foundation1,foundation2,foundation3,foundation4, &resultMessage);
-        } else if (lastCommand[0] == 'Q' && lastCommand[1] == 'Q') {
-            printf("TO DO!\n");
-            break; //replace
-        } else if (lastCommand[0] == 'P') {
-            printf("TO DO!\n");
-        } else if (lastCommand[0] == 'Q') {
-            printf("TO DO!\n");
-        } else if (lastCommand[0] != 0){
-            //Game moves
-            printf("TO DO!\n");
+        if (STARTUP) {
+            if (lastCommand[0] == 'L' && lastCommand[1] == 'D') {
+                result = LD(lastCommand, cardDeck, &resultMessage);
+            } else if (lastCommand[0] == 'S' && lastCommand[1] == 'W') {
+                printf("TO DO!\n");
+            } else if (lastCommand[0] == 'S' && lastCommand[1] == 'L') {
+                printf("TO DO!\n");
+            } else if (lastCommand[0] == 'S' && lastCommand[1] == 'R') {
+                printf("TO DO!\n");
+            } else if (lastCommand[0] == 'S' && lastCommand[1] == 'D') {
+                result = SD(lastCommand, cardDeck, &resultMessage);
+            } else if (lastCommand[0] == 'Q' && lastCommand[1] == 'Q') {
+                printf("TO DO!\n");
+                break; //replace
+            } else if (lastCommand[0] == 'P') {
+                printf("TO DO!\n");
+            } else if (lastCommand[0] == 'Q') {
+                printf("TO DO!\n");
+            } else if (lastCommand[0] != 0) {
+                *resultMessage = "The chosen command does not exist in the STARTUP phase!";
+            }
+        } else {
+            if (lastCommand[0] == 'L' && lastCommand[1] == 'D') {
+                result = L(lastCommand, C1, C2, C3, C4, C5, C6, C7, foundation1, foundation2, foundation3, foundation4,
+                            &resultMessage);
+            } else if (lastCommand[0] == 'S' && lastCommand[1] == 'D') {
+                result = S(lastCommand, C1, C2, C3, C4, C5, C6, C7, foundation1, foundation2, foundation3, foundation4,
+                            &resultMessage);
+            } else {
+                //Game moves
+                printf("TO DO!\n");
+            }
         }
 
         if (lastCommand[0] != 0) {
@@ -92,7 +107,128 @@ int main() {
     return 0;
 }
 
-int LD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4, char **resultMessage) {
+int LD(char lastCommand[], node *cardDeck, char **resultMessage) {
+    if (lastCommand[2] == '<') {
+        char optionalParameter[MAX_STRING];
+        int j = 0;
+        for (int i = 3; i < MAX_STRING; i++) {
+            if (lastCommand[i] == '>') {
+                break;
+            }
+            optionalParameter[j] = lastCommand[i];
+            j++;
+        }
+        optionalParameter[j] = '\0';
+        FILE *infile;
+        infile = fopen(optionalParameter, "r");
+        if (infile != NULL) {
+            node* controlCardDeck = malloc(sizeof(node));
+            controlCardDeck -> next = NULL;
+            insertCardDeck(controlCardDeck);
+
+            int lineCount = 0;
+            char line[5];
+            int i = 0;
+            node* current = cardDeck;
+            while (fgets(line, sizeof(line), infile)) {
+                lineCount++;
+
+                current -> next = malloc(sizeof(node));
+                current = current -> next;
+                current -> next = NULL;
+                current -> suit = line[0];
+                current -> face = line[1];
+                current -> hidden = 1;
+
+                node* currentControl = controlCardDeck;
+                while (currentControl -> next != NULL) {
+                    if (currentControl -> suit == current -> suit && currentControl -> face == current -> face) {
+                        if (currentControl -> hidden == 0) {
+                            currentControl -> hidden = 1;
+                            break;
+                        } else {
+                            sprintf(*resultMessage, "Error in savefile at line %d! Card: %c%c is a duplicate.", lineCount, current -> suit, current -> face);
+                            fclose(infile);
+                            unloadCards(controlCardDeck);
+                            unloadCards(cardDeck -> next);
+                            return -1;
+                        }
+                    }
+                    currentControl = currentControl -> next;
+                }
+                if (currentControl -> next == NULL && currentControl -> suit != current -> suit && currentControl -> face != current -> face) {
+                    sprintf(*resultMessage, "Error in savefile at line %d! Card: %c%c is of illegal format.", lineCount, current -> suit, current -> face);
+                    fclose(infile);
+                    unloadCards(controlCardDeck);
+                    unloadCards(cardDeck -> next);
+                    return -1;
+                }
+            }
+            fclose(infile);
+            if (lineCount != 52) {
+                node* currentControl = controlCardDeck;
+                while (currentControl -> next != NULL) {
+                    if (currentControl -> hidden == 0) {
+                        sprintf(*resultMessage, "Error in savefile! There are not 52 cards as card: %c%c is missing.", currentControl -> suit, currentControl -> face);
+                        unloadCards(controlCardDeck);
+                        unloadCards(cardDeck -> next);
+                        return -1;
+                    }
+                    currentControl = currentControl -> next;
+                }
+            }
+        } else {
+            *resultMessage = "Error. File does not exist!";
+            return -1;
+        }
+    } else {
+        insertCardDeck(cardDeck);
+    }
+    return 0;
+}
+
+int SD(char lastCommand[], node *cardDeck, char **resultMessage) {
+    FILE *outfile;
+    if (lastCommand[2] == '<') {
+        char optionalParameter[MAX_STRING];
+        int j = 0;
+        for (int i = 3; i < MAX_STRING; i++) {
+            if (lastCommand[i] == '>') {
+                break;
+            }
+            optionalParameter[j] = lastCommand[i];
+            j++;
+        }
+        optionalParameter[j] = '\0';
+        outfile = fopen(optionalParameter, "w");
+    } else {
+        outfile = fopen("cards.txt", "w");
+    }
+    if (outfile != NULL) {
+        node* current = cardDeck -> next;
+        while (current != NULL) {
+            fprintf(outfile, "%c%c\n",current->suit,current->face);
+            current = current -> next;
+        }
+        fflush(outfile);
+        fclose(outfile);
+    } else {
+        *resultMessage = "Something went wrong with the save file!";
+        return -1;
+    }
+    return 0;
+}
+
+void unloadCards(node* cards) {
+    node* current = cards;
+    while(current -> next != NULL) {
+        node* temp = current -> next;
+        free(current);
+        current = temp;
+    }
+}
+
+int L(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4, char **resultMessage) {
     if (lastCommand[2] == '<') {
         char optionalParameter[MAX_STRING];
         int j = 0;
@@ -207,7 +343,7 @@ int LD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, nod
     return 0;
 }
 
-int SD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4, char **resultMessage) {
+int S(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, node *C6, node *C7, node *foundation1, node *foundation2, node *foundation3, node *foundation4, char **resultMessage) {
     FILE *outfile;
     if (lastCommand[2] == '<') {
         char optionalParameter[MAX_STRING];
@@ -222,7 +358,7 @@ int SD(char lastCommand[], node *C1, node *C2, node *C3, node *C4, node *C5, nod
         optionalParameter[j] = '\0';
         outfile = fopen(optionalParameter, "w");
     } else {
-        outfile = fopen("cards.txt", "w");
+        outfile = fopen("saveFile.txt", "w");
     }
     if (outfile != NULL) {
         node* current;
